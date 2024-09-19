ee463acfc7199036854423e7a4db9727b1241fd4
Natalia Yankovskaya <Nata-Yanka@yandex.ru>
2024-09-19 16:09:06 +0300

    первая лабораторная

---
+++ лабораторная 1/код/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState } from 'react';
+function App() {
+
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <span>{count}</span> <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+export default App;
